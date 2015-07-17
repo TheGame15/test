23344d514eb57b0675306d97366a9022d70ae102
Guenther Koessler <db.koessler@gmx.at>
2015-07-17 16:45:25 +0200

    test2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int blub;
+
+        blub = 10;
+
+        System.out.println(blub);
     }
 }
